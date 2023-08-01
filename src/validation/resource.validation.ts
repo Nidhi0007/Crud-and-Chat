978b14e857f8c0f78ce44115e78e5f13680b9ab2
Nidhi <bhatnidhis999@gmail.com>
2023-08-02 00:33:42 +0530

    added resource api

--- src/validation/resource.validation.ts
+++ src/validation/resource.validation.ts
@@ -2,10 +2,29 @@ let Joi = require('joi')
 
 const resourceSchema = {
     body: Joi.object().keys({
-        name: Joi.string().email().required(),
+        name: Joi.string().required(),
         description: Joi.string()
     })
 }
+
+const updateResource = {
+    params: Joi.object().keys({
+        id: Joi.string(),
+    }),
+    body: Joi.object().keys({
+        name: Joi.string(),
+        description: Joi.string()
+    })
+}
+const deleteResources = {
+    params: Joi.object().keys({
+        id: Joi.string(),
+    }),
+}
+
+
 export default {
-    resourceSchema
+    resourceSchema,
+    updateResource,
+    deleteResources
 }
