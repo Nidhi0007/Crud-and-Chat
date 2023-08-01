7947419ff44c6a74cdb81ec2c11d75e203a72bc3
Nidhi <bhatnidhis999@gmail.com>
2023-08-01 23:45:13 +0530

    crud for resources

--- src/controller/resources.controller.ts
+++ src/controller/resources.controller.ts
@@ -12,14 +12,41 @@ const addResource = async (req: Request, res: Response) => {
         return res.status(401).json({ message: error });
     }
 }
-const getResource = async () => {
+const getResource = async (req: Request, res: Response) => {
+    try {
+        const findResources = await resourcesModel.find()
+        return res.send(findResources)
+    } catch (error) {
+        return res.status(401).json({ message: error });
+    }
 
 }
-const updateResource = async () => {
+const updateResource = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        const data = req.body
+        const updatedResource = await resourcesModel.findOneAndUpdate({ id: id }, {
+            name: data.name,
+            description: data.description
+        }, {
+            new: true
+        })
+        return res.send({ message: "Resource successfully updated", resource: updatedResource })
+    } catch (error) {
+        return res.status(401).json({ message: error });
+    }
+
 
 }
-const removeResource = async () => {
+const removeResource = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
 
+        await resourcesModel.deleteOne({ id: id })
+        return res.send({ message: "Resource successfully deleted" })
+    } catch (error) {
+        return res.status(401).json({ message: error });
+    }
 }
 export default {
     addResource,
